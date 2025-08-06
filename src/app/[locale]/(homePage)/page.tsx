edb28bb426442cf7803a1019d350b75d4d8f7c1b
mahmouudKhalled <foktop018@gmail.com>
2025-08-06 15:27:23 +0300

    feat/(home-page):change some styles in navbar

--- src/app/[locale]/(homePage)/page.tsx
+++ src/app/[locale]/(homePage)/page.tsx
@@ -1,11 +1,36 @@
 import Footer from "@/components/layout/footer";
 import Navbar from "@/components/layout/header";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useTranslations } from "next-intl";
 
 export default function Home() {
+  // Translation
+  const t = useTranslations();
+
   return (
     <>
       <Navbar />
-      <div className="h-screen flex-center">Home Page</div>
+      <section className="box-container h-screen">
+        <div className="flex gap-10">
+          <div className="hero-content-box flex flex-col items-start gap-6">
+            {/* Heading */}
+            <h2 className="text-6xl font-poppins font-bold">{t("heroSection.heading")}</h2>
+
+            {/* Hero Description */}
+            <p className="">{t("heroSection.description")}</p>
+
+            {/* Search Component */}
+            <div className="w-full relative p-2 bg-main/40 flex gap-2 rounded-full">
+              <Input
+                className="bg-background border-none w-[70%]"
+                placeholder={t("search-input-placeholder")}
+              />
+              <Button className="rounded-full w-[30%]">{t("search")}</Button>
+            </div>
+          </div>
+        </div>
+      </section>
       <Footer />
     </>
   );
